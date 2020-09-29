212fbe1b89651cde8f318d811737debe6d71ae79
brunoduartelima <bruno-sacra@hotmail.com>
2020-09-29 22:55:03 +0000

    A1 corredor 2.0

--- exercise-corredor.py
+++ exercise-corredor.py
@@ -5,7 +5,7 @@ pygame.init()
 window = pygame.display.set_mode([800, 580])
 
 quadX = 100
-quadY = 300
+quadY = 175
 
 movX = 0
 movY = 0
@@ -30,10 +30,14 @@ while True:
         movY = 0
       
   
-  window.fill((200, 200, 200))
+  window.fill((150, 200, 180))
 
   pygame.draw.rect(window, (0, 0, 0),
-    [0, 275, 800, 100]
+    [0, 150, 600, 100]
+  )
+
+  pygame.draw.rect(window, (0, 0, 0),
+    [500, 250, 100, 330]
   )
 
   pygame.draw.rect(window, (255, 255, 255),
@@ -45,14 +49,14 @@ while True:
 
   font = pygame.font.Font('freesansbold.ttf', 150)
 
-  if quadY < 275 or quadY + 50 > 375:
+  if quadY < 150 or quadX + 50 > 600 or (quadY + 50 > 250 and quadX < 500) or (quadY > 250 and quadX < 500):
     text = font.render('PERDEU', True, (255, 0, 0))
     window.blit(text, [100, 50])
     pygame.display.update()
     pygame.time.wait(2000)
     sys.exit()
 
-  if quadX + 50 > 800:
+  if quadY + 50 > 580:
     text = font.render('GANHOU', True, (50, 200, 50))
     window.blit(text, [50, 50])
     pygame.display.update()
